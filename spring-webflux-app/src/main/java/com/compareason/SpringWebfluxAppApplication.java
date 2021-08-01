77c4068309c3cb29885114e7a393f760e7ee3a48
Tahniat Ashraf Priyam <tahniat.ashraf@bkash.com>
2021-08-01 09:08:33 +0600

    First commit

---
+++ spring-webflux-app/src/main/java/com/compareason/SpringWebfluxAppApplication.java
@@ -0,0 +1,13 @@
+package com.compareason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxAppApplication.class, args);
+    }
+
+}
